c25883582fa75f614d5f04ad9923040117f2f06d
Miguel Savignano <m.savignano@stuart.com>
2022-11-06 11:33:30 +0100

    spinnaker client

---
+++ src/spinnaker/SpinnakerClient.js
@@ -0,0 +1,25 @@
+const axios = require('axios')
+
+class SpinnakerClient {
+  constructor(baseURL, token) {
+    this.axios = axios.create({
+      baseURL,
+      maxRedirects: 0,
+      headers: { Authorization: `Bearer ${token}` },
+    })
+  }
+
+  async login() {
+    const { headers } = await this.axios.get(`/login`, { validateStatus: this.validSatusOnRedirect })
+    const cookie = headers['set-cookie'][0]
+    this.axios.defaults.headers = { 'Cookie': cookie }
+
+    return this
+  }
+
+  validSatusOnRedirect(status) {
+    return status >= 200 && status <= 303
+  }
+}
+
+module.exports = { SpinnakerClient }
